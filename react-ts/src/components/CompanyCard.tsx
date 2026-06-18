4b3e653186da25e18df1193e858ce3cbad249af0
mitch-aufiero <altermodussoftware@gmail.com>
2026-06-18 18:37:17 -0500

    folder name update

--- react-ts/src/components/CompanyCard.tsx
+++ react-ts/src/components/CompanyCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import type { Company } from "../types/CompanyInterfaces";
+import type { Company } from "../interfaces/CompanyInterfaces";
 
 interface CompanyCardProps{
     
